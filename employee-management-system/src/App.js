6247925d780a0cf5611245d26408d1203b29ca14
Bhushan0704 <bhushanschaudhari2023@gmail.com>
2023-04-28 01:46:13 +0530

    feat: Display the list of employees user

--- employee-management-system/src/App.js
+++ employee-management-system/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
 //Functional component is just a simple javascript function; it accepts the data in the form of props and returns the react element
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Employee Management System !</h1>
-      </header>
+    <div className="container">
+      <ListEmployeeComponent />
     </div>
   );
 }
